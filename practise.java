1a00119a6d988a94167451ee936c600cb2615210
stharamila <98005719+stharamila@users.noreply.github.com>
2022-01-19 21:29:55 -0600

    Add files via upload

---
+++ practise.java
@@ -0,0 +1,12 @@
+
+public class Student {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//local variables
+int sno=100;
+int y=5;
+System.out.println(y);
+	}
+
+}
