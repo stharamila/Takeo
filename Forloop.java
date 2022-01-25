ae9ca03abb536268ebe390724fc502c804ee73ca
stharamila <98005719+stharamila@users.noreply.github.com>
2022-01-24 19:19:35 -0600

    Add files via upload

---
+++ Forloop.java
@@ -0,0 +1,16 @@
+public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//WAP to print star and A pattern
+    
+    
+		for (int i=0; i<5 ; i++)
+		{
+			for (int j=0; j<5 ; j++)
+			{
+				System.out.print("* " );
+			}
+			System.out.println();
+			
+		}
+
+		
